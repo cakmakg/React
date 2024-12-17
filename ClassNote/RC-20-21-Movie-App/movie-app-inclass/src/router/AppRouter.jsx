0e60d82c2c0e46f6eca51e21c955b4a4a14ffaad
cakmakg <cakmak4834@gmail.com>
2024-12-17 15:44:00 +0100

    movie-app

--- ClassNote/RC-20-21-Movie-App/movie-app-inclass/src/router/AppRouter.jsx
+++ ClassNote/RC-20-21-Movie-App/movie-app-inclass/src/router/AppRouter.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
+import Register from '../pages/Register'
 import Login from "../pages/Login";
-import Register from "../pages/Register"
 
 import Main from "../pages/Main";
-import Navbar from "../components/Navbar"
-import { Route, Routes} from "react-router-dom"
-
+import Navbar from "../components/Navbar";
+import {  Route, Routes } from "react-router-dom";
 const AppRouter = () => {
   return (
     <>
 <Navbar/>
 <Routes>
-  <Route path='/' element={<Main/>}/>
-  <Route path='/login' element={<Login/>}/>
-  <Route path='/register' element={<Register/>}/>
-  
-</Routes>
+<Route path="/" element={<Main/>}/>
+<Route path="/login" element={<Login/>}/>
+<Route path="/register" element={<Register/>}/>
 
+
+</Routes>
 
     </>
   )
